06c42392e9f289bca8ccc0a84ce817eef11bc247
mlg47 <107060046+MaxGer99@users.noreply.github.com>
2022-12-27 15:07:05 -0500

    Major changes

--- frontEndChallenge/src/app/app.component.ts
+++ frontEndChallenge/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontEndChallenge';
+
+  // For Stepper
+  isLinear = true;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+  thirdFormGroup!: FormGroup;
+  fourthFormGroup!: FormGroup;
+
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      location: ['', Validators.required],
+      timezone: ['', Validators.required]
+    });
+
+    this.secondFormGroup = this._formBuilder.group({
+      range1: ['', Validators.required],
+      range2: ['', Validators.required]
+    });
+
+    this.thirdFormGroup = this._formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+
+    this.fourthFormGroup = this._formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  submit(){
+      console.log(this.firstFormGroup.value + this.secondFormGroup.value);
+  }
 }
--- frontEndChallenge/src/app/app.module.ts
+++ frontEndChallenge/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
